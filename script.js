d9f0f1f44132dd3a1832e6b6204f4da54faa1cba
Keanunovaa <152877297+Keanunovaa@users.noreply.github.com>
2023-12-05 02:58:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
+yesBtn.addEventListener("click", () => {
+ question.innerHTML = "Aaaaa, ku juga sayangg sama beboooo";
+ gif.src =
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+const noBtnRect = noBtn.getBoundingClientRect();
+const maxX = window.innerWidth - noßtnRect.width;
+const maxY = window.innerHeight - noBtnRect.height;
+  
+const randomX = Math.floor(Math.random() * maxx);
+const randomY = Math.floor(Math.random() * maxy);
+
+noBtn.style.left = randomX + "px";
+noßtn.style.top = randomY + "px"; 
+});
